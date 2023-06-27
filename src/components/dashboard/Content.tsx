649897359908c5fcdd7c22e02f066ec16c447a6d
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2023-06-27 10:57:32 +0330

    feat: add some ui

--- src/components/dashboard/Content.tsx
+++ src/components/dashboard/Content.tsx
@@ -9,8 +9,12 @@ interface IProps {
 const Content = ({ toggleAside }: IProps): JSX.Element => {
   return (
     <ProtectedRoute>
-      <main className={`p-4 ${toggleAside ? "sm:mr-64" : ""}`}>
-        <div className="mt-14">
+      <main
+        className={`p-4 ${
+          toggleAside ? "transition-all duration-200 ease-in-out md:mr-64" : ""
+        }`}
+      >
+        <div className="mt-14 p-4">
           <Outlet />
         </div>
       </main>
