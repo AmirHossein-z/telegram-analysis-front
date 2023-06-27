31de371339b684ecd245314cde093345602a3c55
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2023-06-27 10:18:13 +0330

    refactor: change location of protectedRoute comp

--- src/components/dashboard/Content.tsx
+++ src/components/dashboard/Content.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { ProtectedRoute } from "../../containers";
+import { ProtectedRoute } from "../../routes";
 import { JSX } from "react";
 
 interface IProps {
