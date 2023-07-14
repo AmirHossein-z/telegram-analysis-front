89cc96243963b5644ae5fea782392fc6a67e55da
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2023-07-14 11:03:53 +0330

    add error boundary for fallback UI

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as RegisterPage } from "./RegisterPage";
 export { default as LoginPage } from "./LoginPage";
 export { default as NotFoundPage } from "./NotFoundPage";
 export { default as DashboardPage } from "./DashboardPage";
+export { default as ErrorPage } from "./ErrorPage";
